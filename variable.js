23409160908b8a4d599213044dd5fab2351d7f42
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-05 01:08:25 +0200

    Add exercise 2 variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+// 1. Declare a variable ‘favoriteColour’ and assign your favourite colour to it.
+let favoriteColour = 'black'
+
+// 2. Print the value of favoriteColour.
+console.log(favoriteColour)
+
+// 3. Reassign favouriteColourto a different colour.
+favoriteColour = 'white'
+
+// 4. Print the new value of favouriteColour.
+console.log(favoriteColour)
