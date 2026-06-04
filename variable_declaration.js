e37fe5a827b139c8962f874e513109e23ebfb7ec
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-05 01:04:44 +0200

    Add  exercise 1 variables

---
+++ variable_declaration.js
@@ -0,0 +1,11 @@
+// 1. Declare a variable ‘name’ and assign your name to it.
+let name = 'mothibi'
+
+// 2. Declare a variable ‘age’ and assign your age to it.
+let age = '23'
+
+// 3. Declare a variable ‘isStudent’ and assign it to a boolean value indicatingwhether you are a student or not.
+let isStudent = true
+
+// 4. Print all three variables
+console.log(name, age, isStudent)
